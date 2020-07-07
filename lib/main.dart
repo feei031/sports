9e8e2bc0a356d558e53a815f9d95316c1682c636
feei031 <arkt03111@gmail.com>
2020-07-07 23:53:04 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.green,
+      ),
       home: Scaffold(
           appBar: AppBar(
-            title: const Text('Feel Free Sports'),
+            centerTitle: false,
+            leading: Icon(Icons.directions_run),
+            title: const Text(
+                'Feel Free Sports',
+            ),
+            actions: <Widget>[
+              SizedBox(
+                width: 55,
+                child: FlatButton(
+                  child: Icon(Icons.search,color: Colors.white),
+                  onPressed: (){
+                    //押したときの動き
+                  },
+                ),
+              ),
+              SizedBox(
+                width: 55,
+                child: FlatButton(
+                  child: Icon(Icons.more_vert,color: Colors.white),
+                  onPressed: (){
+                    //押したときの動き
+                  },
+                ),
+              ),
+            ],
           ),
         body: Container(),
       ),
